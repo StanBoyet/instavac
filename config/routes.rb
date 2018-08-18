5caf390983cb72c9d5f076de14c74303bf85678a
Stanislas Boyet <stanislas@boyet.me>
2018-08-18 14:50:12 +0200

    update host and production storage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  default_url_options(:host => "127.0.0.1:3000")
+  default_url_options(:host => "instavac.herokuapp.com")
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
